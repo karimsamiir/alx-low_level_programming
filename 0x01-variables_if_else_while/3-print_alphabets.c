ebc414f7ff342230d3838ce82cce37f1644dacb4
karimsamiir <karimsamir010r@gmail.com>
2023-06-16 16:14:42 -0700

    commit

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,1 +1,32 @@
+#include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Description: print alphabet in lowercase then in uppercase
+ *
+ * Retutn: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char ch = 'a';
+	char CH = 'A';
+
+	/*prints a - z*/
+	while (ch <= 'z')
+	{
+		puchar(ch);
+		ch++;
+	}
+
+	/*print A -Z*/
+	while (CH <= 'Z')
+	{
+		putchar(CH);
+		CH++;
+	}
+	putchar('\n');
+
+	return (0);
+}

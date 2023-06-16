58afce0d69a9740db6aaf966c64def41d8d2a1a5
karimsamiir <karimsamir010r@gmail.com>
2023-06-15 17:11:58 -0700

    commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+
+/**
+ * main  - entry point
+ * Description: c language that prints with put function
+ *
+ * return: 0 (success)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}

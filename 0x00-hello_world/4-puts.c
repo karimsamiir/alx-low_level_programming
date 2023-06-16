3c4e93b98100d2085e6d38e6e89170fdd9a5e693
karimsamiir <karimsamir010r@gmail.com>
2023-06-15 17:23:56 -0700

    commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,11 +4,8 @@
  * main  - Entry point
  * Description: A c language that prints with put function
  *
-<<<<<<< HEAD
- * Return: 0 (success)
-=======
+ * Return: 0 (succss)
  * return: Always 0 (success)
->>>>>>> a714a8aab5e7ae816a41116450e7866e217523d7
  */
 
 int main(void)

f817cc19c641a26d972fde39c36f5548b308c686
karimsamiir <karimsamir010r@gmail.com>
2023-06-27 14:39:41 -0700

    commit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,8 @@
 #include"main.h"
 
 /**
- * print_alphabet - A function that prints the alphabet, in lowercase, followed by a new line
+ * print_alphabet - A function that prints the alphabet,
+ *		 in lowercase, followed by a new line
 */
 
 

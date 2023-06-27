9c4bd72f923ec88dd56481c52539a1086735ddad
karimsamiir <karimsamir010r@gmail.com>
2023-06-27 16:08:11 -0700

    commit

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -5,13 +5,13 @@
  *		 absolute value of an intege
  *
  *@n: take intrger type input for absolute
- * 
+ *
  * Return: Always 0 (success)
 */
 
 int _abs(int n)
 {
 	if (n < 0)
-		n = (-1) *n;
+		n = (-1) * n;
 	return (n);
 }

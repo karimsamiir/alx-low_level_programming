e84d285ce55d09e32296f66cc666b216adcbb5c3
karimsamiir <karimsamir010r@gmail.com>
2023-06-27 15:17:52 -0700

    commit

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _isalpha - A  function that checks for 
+ *		alphabetic character
+ * 
+ * @c: takes input from other functions
+ *
+ * Return: 1 if c is true
+ *		else return 0 (success)
+*/
+
+int _isalpha(int c);
+{
+	if (c >= 97 && c <= 122)
+		return (1);
+	if (c >= 65 && c <= 90)
+		return (1);
+	return (0);
+}

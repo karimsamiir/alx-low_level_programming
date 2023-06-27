1ec87d6cd92f8fdad7ef84574b81ae961a29c853
karimsamiir <karimsamir010r@gmail.com>
2023-06-27 14:47:27 -0700

    commit

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include"main.h"
+
+/**
+ * print_alphabet_x10 - A  function that prints 10 times the alphabet,
+ *			 in lowercase, followed by a new line
+*/
+
+void print_alphabet_x10(void)
+{
+	int line, ch;
+
+	for (line = 0; line <= 9; line++)
+	{
+		for (ch = 'a' ch <= 'z';ch++)
+			_putchar(ch);
+		_putchar('\n');
+	}
+}

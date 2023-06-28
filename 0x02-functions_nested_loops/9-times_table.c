ad6e016bd45346f75cddd4bcef8df67f7dc668ed
karimsamiir <karimsamir010r@gmail.com>
2023-06-27 18:13:12 -0700

    commit

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,7 +9,7 @@ void times_table(void)
 {
 	int num, mult, prod;
 
-	for (num =0; num <= 9; mult++)
+	for (num = 0; num <= 9; mult++)
 	{
 		_putchar(48);
 		for (mult = 1; mult <= 9; mult++)
@@ -22,9 +22,10 @@ void times_table(void)
 			if (prod <= 9)
 				_putchar(' ');
 			else
-				_putchar((prod / 10) + 48);
-			_putchar((prod % 10) + 48);
+				_putchar((prod / 10) + 48)
+			
+			_putchar((prod % 10) + 48)
 		}
-		_putchar('\n');
+		_putchar('\n')
 	}
 }

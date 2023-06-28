2d20d1bc6ff0810a0716285626da6900ac56d9be
karimsamiir <karimsamir010r@gmail.com>
2023-06-27 17:44:09 -0700

    commit

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -24,8 +24,8 @@ void times_table(void)
 			else
 				_putchar((prod / 10) + 48);
 
-			_putchar((prod / 10) + 48)i;
+			_putchar((prod / 10) + 48;
 		}
-	}	_putchar('\n')i;
+	}	_putchar('\n');
 }
 

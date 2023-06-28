196a9b8af8dcc9deab11a5dabab406b07bf547e6
karimsamiir <karimsamir010r@gmail.com>
2023-06-27 20:04:51 -0700

    commit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include"main.h"
+/**
+ * add - A unction that adds two integers
+ *		 and returns the result
+ *
+ * @a: firstinput
+ * @b: second input
+ *
+ * Return: sum of (a & b)
+*/
+
+int add(int a, int b)
+{
+	int sum = a + b;
+
+	return (sum);
+}

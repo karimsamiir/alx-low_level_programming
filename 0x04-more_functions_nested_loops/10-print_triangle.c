597a665d80bfebaa73a80029236cf29390307f8f
karimsamiir <karimsamir010r@gmail.com>
2023-06-29 07:10:58 -0700

    commit

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,32 @@
+#include"main.h"
+
+/**
+ * print_triangle - A  function that prints a triangle,
+ *			followed by a new line
+ * 
+ * @size: size of the triangle
+ *
+ * Return: Always 0 (success)
+*/
+
+void print_triangle(int size)
+{
+	int hight, base;
+
+	if (size <= 0)
+		_putchar('\n');
+	else
+	{
+		for (hight = 1; hight <= size; hight++)
+		{
+			for (base = 1; base <= size; base++)
+			{
+				if ((hight + base) <= size)
+					_putchar(' ');
+				else
+					_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}

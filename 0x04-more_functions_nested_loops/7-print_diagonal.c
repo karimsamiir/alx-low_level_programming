e0169eb0d00bea06627f44f052cdf34182745515
karimsamiir <karimsamir010r@gmail.com>
2023-06-28 18:13:26 -0700

    commit

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -15,11 +15,13 @@ void print_diagonal(int n)
 	else
 	{
 		for (var = 1; var <= n; var++)
+                {
+			for (kir = 1; kir <= var; kir++)
+				_putchar(' ');
+			_putchar(92);
+			_putchar('\n');
+                }
 	}
-		for (kir = 1; kir <= var; kir++)
-			_putchar(' ');
-		_putchar(92);
-		_putchar('\n');
 }
 
 

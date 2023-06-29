4104589e753cea3e6a06a47a304aaf67dd51a900
karimsamiir <karimsamir010r@gmail.com>
2023-06-29 15:43:26 -0700

    commit

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,28 @@
+#include"main.h"
+
+/**
+ * _strncat - A function that concatenates two strings
+ *
+ * @dest: pointer to destenation input
+ * @src: pointer to source input
+ * @n: most num of bytes from @src
+ *
+ * Return: @dest
+*/
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int c, i;
+
+	c = 0;
+
+	while (dest[c])
+		c++;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[c = i] = src[i];
+
+	dest[c + i] = '\0';
+
+	return (dest);
+}

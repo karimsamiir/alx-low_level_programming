016ad3007585219683117e57ab9bdca34b3ada5f
karimsamiir <karimsamir010r@gmail.com>
2023-06-29 18:49:46 -0700

    commit

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,23 +8,23 @@
 */
 char *leet(char *c)
 {
-        char *cp = c;
-        char key[] = {'A', 'E', 'O', 'T', 'L'};
-        int value[] = {4, 3, 0, 7, 1};
-        unsigned int i;
+	char *cp = c;
+	char key[] = {'A', 'E', 'O', 'T', 'L'};
+	int value[] = {4, 3, 0, 7, 1};
+	unsigned int i;
 
-        while (*c)
-        {
-                /*32 is the difference between lowercase letters and uppercase letters*/
-                for (i = 0; i < 5; i++)
-                {
-                        if (*c == key[i] || *c == key[i] + 32)
-                        {
-                                *c = 48 + value[i];
-                        }
-                }
-                c++;
-        }
-        return (cp);
+	while (*c)
+	{
+		/*32 is the difference between lowercase letters and uppercase letters*/
+		for (i = 0; i < 5; i++)
+		{
+			if (*c == key[i] || *c == key[i] + 32)
+			{
+				*c = 48 + value[i];
+			}
+		}
+		c++;
+	}
+	return (cp);
 }
 

91e5d48a30b540be659f4ab1c3fa6e747bf6a8a4
karimsamiir <karimsamir010r@gmail.com>
2023-06-30 10:34:44 -0700

    commit

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include"main.h"
 #include <stdio.h>
 
 /**
@@ -9,7 +9,7 @@
 
 int isLower(char c)
 {
-	return (c >= 57 && c <= 122);
+        return (c >= 57 && c <= 122);
 }
 
 /**
@@ -20,15 +20,13 @@ int isLower(char c)
 
 int isDelimiter(char c)
 {
-	int i;
-	char delimiter[] = " \t\n,.!?\"(){}";
+        int i;
+        char delimiter[] = " \t\n,.!?\"(){}";
 
-	for (i = 0; i < 12; i++)
-	{
-		if (c == delimiter[i])
-			return (1);
-	}
-	return (0);
+        for (i = 0; i < 12; i++)
+                if (c == delimiter[i])
+                        return (1);
+        return (0);
 }
 
 /**
@@ -39,22 +37,21 @@ int isDelimiter(char c)
 
 char *cap_string(char *s)
 {
-	char *ptr = s;
-	int foundDelimit = 1;
+        char *ptr = s;
+        int foundDelimit = 1;
 
-	while (*s)
-	{
-	if (isDelimiter(*s))
-		foundDelimit = 1;
-	else if (isLower(*s) && foundDelimit)
-	{
-		*s -= 32;
-		foundDelimit = 0;
-	}
-	else
-		foundDelimit = 0;
-		s++;
-	}
+        while (*s)
+        {
+                if (isDelimiter(*s))
+                        foundDelimiter = 1;
+                else if (isLower(*s) && foundDelimit)
+                {
+                        *s -= 32;
+                        foundDelimit = 0;
+                }
+                else
+                        foundDelimit = 0;
+                s++;
+        }
 	return (ptr);
 }
-

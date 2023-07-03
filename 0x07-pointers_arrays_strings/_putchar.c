135dbfca88d5e00c19d3368babe8a0c19519946f
karimsamiir <karimsamir010r@gmail.com>
2023-07-03 10:46:13 -0700

    commit

--- 0x07-pointers_arrays_strings/_putchar.c
+++ 0x07-pointers_arrays_strings/_putchar.c
@@ -1,8 +1,15 @@
-#include <unisted.h>
+#include <unistd.h>
+#include "main.h"
 
 /**
  * _putchar - writes the character c to stdout
- * @c: the character to print
+ * @c: The character to print
  *
- * Return: on success 1
- * on error -1 is returned*/
+ * Return: on (success) 1.
+ *	on error -1 will be returned
+*/
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}

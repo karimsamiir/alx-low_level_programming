58390478b8190679a899662b48ee127aa793da61
karimsamiir <karimsamir010r@gmail.com>
2023-07-03 11:21:43 -0700

    commit

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,24 @@
+#include"main.h"
+
+/**
+ * _strspn - A function that gets the length of a prefix substring
+ * @s: string
+ * @accept: bytes
+ * Return: unsigned int
+*/
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int com, tom;
+
+	for (com = 0; s[com] != '\0'; com++)
+	{
+		for (tom = 0; accept[tom] != s[tom]; tom++)
+		{
+			if (accept[tom] == '\0')
+				return (tom);
+		}
+	}
+
+	return (com);
+}

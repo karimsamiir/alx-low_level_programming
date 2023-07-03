aed132ce19c63ac0faf35c8f417b641cb9db1186
karimsamiir <karimsamir010r@gmail.com>
2023-07-03 07:23:34 -0700

    commit

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,4 +1,21 @@
 #include"main.h"
 
 /**
- * */
+ * *_memcpy - copy memory
+ * @dest: memory
+ * @src: source
+ * @n: the length copied from src
+ *
+ * Return: pointer to dest
+*/
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int com;
+
+	for (com = 0; com < n; com++)
+	{
+		dest[com] = src[com];
+	}
+	return (dest);
+}

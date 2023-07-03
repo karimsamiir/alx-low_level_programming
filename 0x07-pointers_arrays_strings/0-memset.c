de3e7b0e464a56a520809d088128ed89f8db1c0f
karimsamiir <karimsamir010r@gmail.com>
2023-07-03 07:18:13 -0700

    commit

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _memset - A function that fills memory with a constant byte
+ * *_memset - A function that fills memory with a constant byte
  * @s: pointer
  * @b: constant
  * @n: max bytes for using

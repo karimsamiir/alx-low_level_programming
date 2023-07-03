6f95bfc9e1e59a77fbd0b9e2fc8d02520bdfda59
karimsamiir <karimsamir010r@gmail.com>
2023-07-03 11:36:31 -0700

    commit

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,7 +1,7 @@
 #include"main.h"
 
 /**
- * *_strpbrk - A function that searches a string for any of a set of bytes
+ * _strpbrk - A function that searches a string for any of a set of bytes
  * @s: string
  * @accept: mathing string
  *

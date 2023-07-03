d4cca0df2467a0048e2fc4786367ee0c896f6c00
karimsamiir <karimsamir010r@gmail.com>
2023-07-03 11:10:39 -0700

    commit

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * *_strchr - A function that locates a character in a string
+ * @s: pointer to put the constsnt
+ * @c: constsnt
+ * Return: pointer to s
+*/
+
+char *_strchr(char *s, char c)
+{
+	int com;
+
+	for (com =0; s[com] >= '\0' ; com++)
+	{
+		if (s[com] == c)
+		{
+			return (s + com);
+		}
+	}
+
+	return ('\0');
+}

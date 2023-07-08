52122ad15d18ef20c5579000f4a73beb6d4d8182
karimsamiir <karimsamir010r@gmail.com>
2023-07-07 17:27:40 -0700

    commit

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -3,8 +3,8 @@
 /**
  * main - A program that prints the number of arguments passed into it
  *
- * com: num of command line
- * tom: pointer to an array of commandline
+ * com: num of command line arguments
+ * tom: pointer to an array of commandline arguments
  *
  * Return: 0(success), not 0 error
 */

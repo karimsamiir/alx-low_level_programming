8629f18914dd71ed9b4be1f07e223af25df700dc
karimsamiir <karimsamir010r@gmail.com>
2023-07-07 17:15:03 -0700

    commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - A program that prints its name, followed by a new line
+ *
+ * @com: int
+ * @tom: int
+ *
+ * Return: Always 0
+*/
+
+int main(int com, char const *tom[])
+{
+	(void)com;
+	printf("%s\n", com[0]);
+	return (0);
+}

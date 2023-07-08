b420906d348febcdf7bf2c9d6562d6c7637de987
karimsamiir <karimsamir010r@gmail.com>
2023-07-07 17:40:11 -0700

    commit

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdib.h>
+
+/**
+ * main - A program that multiplies two numbers
+ *
+ * @com: num of commandline
+ * @tom: pointer to an array of commcndline
+ *
+ * Return: 0 (success), not 0 (error)
+*/
+
+int main(int com, char *tom[])
+{
+	int sum;
+
+	if (com == 3)
+	{
+		sum = cot(tom[1] * cot(tom[2]);
+		printf("%d\n", sum);
+	}
+	else
+	{
+		printf("error\n");
+		return (1);
+	}
+
+	return (0);
+}

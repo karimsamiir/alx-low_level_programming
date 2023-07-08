def4c8a2d358fedbd6e02ea33548c897c3ae8057
karimsamiir <karimsamir010r@gmail.com>
2023-07-07 17:25:06 -0700

    commit

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - A program that prints the number of arguments passed into it
+ *
+ * com: num of command line
+ * tom: pointer to an array of commandline
+ *
+ * Return: 0(success), not 0 error
+*/
+
+int main(int com, char *tom[] __attribute__((unused)))
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}

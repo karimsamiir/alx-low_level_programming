1ea0b88dc24fcdf64b79a65052bb46090ce46fe4
karimsamiir <karimsamir010r@gmail.com>
2023-07-17 08:06:25 -0700

    commit

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a dog struct
+ *
+ * @name: the dog name
+ * @age: the dog age
+ * @owner: the dog owner
+ *
+ * Desciption: just a lone dog struct in a big kitty world
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif

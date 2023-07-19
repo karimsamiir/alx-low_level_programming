80b997a972f9652292c099082a3e1aa9cf7935f9
karim samir EN9 <120334616+karimsamiir@users.noreply.github.com>
2023-07-19 19:47:10 +0300

    Update 3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,5 +1,4 @@
 #include "3-calc.h"
-
 /**
  * main - check code for school students
  *
@@ -7,25 +6,25 @@
  * @argv: arg vector
  *
  * Return: Always 0 (success)
-*/
+ */
 
 int main(int argc, char **argv)
 {
-	int (*op_func)(int, int), a, b;
+    int (*op_func)(int, int), a, b;
 
-	if (argc != 4)
-		printf("Error\n"), exit(98);
+    if (argc != 4)
+        printf("Error\n"), exit(98);
 
-	a = atoi(argv[1]);
-	b = atoi(argv[3]);
+    a = atoi(argv[1]);
+    b = atoi(argv[3]);
 
-	op_func = get_op_func(argv[2]);
-	if (!op_func)
-		printf("Error\n"), exit(99);
+    op_func = get_op_func(argv[2]);
+    if (!op_func)
+        printf("Error\n"), exit(99);
 
-	if (!b && (argv[2][0] == '/' || argv[2][0] == '%'))
-		printf("Erorr\n"), exit(100);
+    if (!b && (argv[2][0] == '/' || argv[2][0] == '%'))
+        printf("Error\n"), exit(100);
 
-	printf("%d\n", op_func(a, b));
-	return (0);
+    printf("%d\n", op_func(a, b));
+    return (0);
 }

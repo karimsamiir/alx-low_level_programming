c5153c58e9fddf2d6f9a2e8d7153b7b67e4e584a
karimsamiir <karimsamir010r@gmail.com>
2023-07-19 14:03:03 -0700

    commit

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,59 @@
+#include "3-calc.h"
+
+/**
+ * op_add - add 2 intergers
+ *
+ * @a: first int
+ * @b: second int
+ *
+ * Return: sum
+*/
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - subtract 2 intergers
+ *
+ * @a: first int
+ * @b: second int
+ *
+ * Return: difference
+*/
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_div - divides 2 intergers
+ *
+ * @a: first int
+ * @b: second int
+ *
+ * Return: quoient
+*/
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - module 2 intergers
+ *
+ * @a: first int
+ * @b: second int
+ *
+ * Return: remainder
+*/
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
+
+

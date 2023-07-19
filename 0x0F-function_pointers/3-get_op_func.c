fb739bfa4efc81767a3ff994d3184faf7deadf0e
karim samir EN9 <120334616+karimsamiir@users.noreply.github.com>
2023-07-19 19:45:54 +0300

    Update 3-get_op_func.c

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,4 +1,4 @@
-#include"3-calc.h"
+#include "3-calc.h"
 
 /**
  * get_op_func - return the pointer to the right op func
@@ -6,26 +6,26 @@
  * @s: string operator
  *
  * Return: the right function pointer
-*/
+ */
 
 int (*get_op_func(char *s))(int, int)
 {
-	op_t ops[] = {
-		{"+", op_add},
-		{"-", op_sub},
-		{"*", op_mul},
-		{"/", op_div},
-		{"%", op_mod},
-		{NULL, NULL}
-	};
-	int i = 0;
+    op_t ops[] = {
+        {"+", op_add},
+        {"-", op_sub},
+        {"*", op_mul},
+        {"/", op_div},
+        {"%", op_mod},
+        {NULL, NULL}
+    };
+    int i = 0;
 
-	while (i < 5)
-	{
-		if (s && s[0] == ops[i].op[0] && !s[1])
-			return (ops[i].f);
-		i++;
-	}
+    while (i < 5)
+    {
+        if (s && s[0] == ops[i].op[0] && !s[1])
+            return (ops[i].f);
+        i++;
+    }
 
-	return (NULL);
+    return (NULL);
 }

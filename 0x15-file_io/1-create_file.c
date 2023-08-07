5560864a308b367d24e9b41abb067b2ff9aa8858
karimsamiir <karimsamir010r@gmail.com>
2023-08-07 16:10:04 -0700

    kariim

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -36,11 +36,11 @@ int create_file(const char *filename, char *text_content)
 
 	if (!filename)
 		return (-1);
-	kim = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUR | S_IWUSR);
+	kim = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (kim == -1)
 		return (-1);
 	if (lenght)
 		byte = write(kim, text_content, lenght);
 	close(kim);
-	return (bytes == lenght ? 1 : -1);
+	return (byte == lenght ? 1 : -1);
 }
